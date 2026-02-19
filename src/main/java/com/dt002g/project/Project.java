b726d4e55a32a087a1a35aa5b96b239687c3a4c9
Rasmus Sandahl <rasa2300@student.miun.se>
2026-02-19 21:38:21 +0100

    initial skeleton

---
+++ src/main/java/com/dt002g/project/Project.java
@@ -0,0 +1,18 @@
+package com.dt002g.project;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * The main starting point for Project.
+ */
+public final class Project {
+    private Project() { throw new IllegalStateException("Utility class"); }
+
+    /**
+     * Main program entrypoint.
+     * @param args command arguments.
+     */
+    public static void main(final String... args) {
+        System.out.println("helo");
+    }
+}
---
+++ src/test/java/com/dt002g/project/ProjectTest.java
@@ -0,0 +1,18 @@
+package com.dt002g.project;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Main test class for unit testing Project.
+ */
+public class ProjectTest {
+    /**
+     * Dummy test case, remove once a real test case is implemented.
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
